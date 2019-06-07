ecf11ee36bc07c52d3e19168fa03ee89fee82025
patriclucescu <patricklucescu@gmail.com>
2019-06-07 21:20:52 +0200

    added comments

--- optimize_pf.py
+++ optimize_pf.py
@@ -5,66 +5,25 @@ from scipy import sparse
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import covariance as cv
-import LSTM as ls
 import HMM as hmm
 
 
-def read_batch_multi(X, batch_size, future=10, nr_taps=2, batch_start=0):
-    num_features = 1
-    s = X
-    x = np.zeros((num_features, batch_size, s.shape[1]*nr_taps))
-    y = np.zeros((num_features, batch_size, s.shape[1]))
-    for i in range(nr_taps):
-        x[0, :, i * s.shape[1]:(i + 1) * s.shape[1]] = s.iloc[i:-future - nr_taps + i + 1, :]
-    y[0, :, :] = s.rolling(future).mean().iloc[future+nr_taps-1:, :]
-    return x, y
-
-
-def RNNLSTM(X):
-
-    def generate_batch(N):
-        assert(X.shape[0] > N)
-        x, y = read_batch_multi(X, N, future=future, nr_taps=nr_taps)
-        ys = 0.1*y + 0.9*y.mean(axis=2)[:, :, np.newaxis]       # shrinkage
-
-        return x, ys
-
-    future = 50
-    nr_taps = 5
-    x_s = X.shape[1]     # columns
-
-    model = ls.Model(input_size=nr_taps*x_s, output_size=x_s)
-    model.build()
-    for epoch in range(10):
-        epoch_error = model.train_batch(generate_batch=generate_batch, batch_size=X.shape[0]-future-nr_taps+1)
-        print(epoch_error)
-    last_x = np.zeros((1, 1, nr_taps * x_s))
-    for i in range(nr_taps):
-        last_x[0, 0, i*x_s:(i+1)*x_s] = X.iloc[-nr_taps+i, :]
-
-    out = model.predict_batch(last_x)
-
-    return out[0].ravel()
-
-def HMM(X):
-    K = 4
-    p = .1
-    iter = 40
+def HMM(X,K,p,iter):
     posteriori_prob, mu_s, cov_s, pred = hmm.expectation_maximization(X, K, iter, p)
 
     return pred
 
 def optimize(x, ra, method=None):
+    ## SET HMM PARAMETERS
+    K = 4
+    p = 0.1
+    iter = 50
 
     ret = x.mean().fillna(0).values
 
     cov = ra * pd.DataFrame(data=cv.oas(x)[0],index=x.columns, columns=x.columns).fillna(0)
-    
-    if method is 'HMM':
-        ret = HMM(x)
 
-    if method is 'LSTM_Multi':
-        ret = RNNLSTM(x)
+    ret = HMM(x,K,p,iter)
 
     problem = osqp.OSQP()
     k = len(ret)
@@ -93,7 +52,6 @@ def optimize(x, ra, method=None):
 if __name__ == '__main__':
 
     # load data ###################################################################
-    method = 'HMM'
     risk_aversion = 1 # I assume is for the Mean Variance Optimization
     window = 150 # after how many weeks do you want to rebalance
 
@@ -112,17 +70,17 @@ if __name__ == '__main__':
     for date in dtindex[window - 1:]:
         today = date
         returns = input_returns.loc[:today, :] #.tail(window)
-        last = returns.index[-2]
+        last = returns.index[-1]
 
         if today in rebalancing_dates:  # re-optimize and get new weights
             print(today)
-            weights.loc[today, :] = optimize(returns, risk_aversion, method)
+            weights.loc[today, :] = optimize(returns, risk_aversion)
         else:  # no re-optimization, re-balance the weights
             weights.loc[today, :] = weights.loc[last, :] * (1 + returns.loc[today, :]) \
                                     / (1 + (weights.loc[last, :] * returns.loc[today, :]).sum())
 
     pnl = (weights.shift(1)*input_returns).sum(axis=1)
-
+    one = np.ones(964)
     plt.figure
     pnl.cumsum().plot()
     plt.title('Sharpe : {:.3f}'.format(pnl.mean()/pnl.std()*np.sqrt(52)))
