ce037c9f4f443f9377421db581597122e51913e8
patriclucescu <patricklucescu@gmail.com>
2019-06-11 22:29:59 +0200

    Put everything in a clean way

---
+++ data_processing.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+def database_processing(df, dtindex,asset_removal):
+    """ Reindex dataset by dtindex and clean the dataset of unwanted assets(if asset_removal is true)"""
+    df0 = pd.DataFrame(data=df.values, columns=df.columns, index=pd.to_datetime(df['Date'], format='%d/%m/%Y'))
+    df0 = df0.reindex(dtindex)
+    df0 = df0.drop(columns=['Date'])
+
+    if asset_removal:
+        for asset in df.columns[1:]:
+            if pd.isna(df0.loc[dtindex[0]][asset]):
+                df0 = df0.drop(columns=[asset])
+        return df0
+    else:
+        return df0
+
+def returns_cleaning(input_returns, df, rolling_window, today):
+    """ If rolling window is available, remove assets that do not have enough observations
+     If not, just remove assets that do not have at least one return available"""
+    available_assets = []
+    if rolling_window:
+        for asset in input_returns.columns[1:]:
+            if all(df.loc[today - rolling_window:today][asset] > 0):
+                available_assets.append(asset)
+        returns = input_returns.loc[today - rolling_window:today, available_assets]
+
+    else:
+        for asset in input_returns.columns[1:]:
+            if df.loc[today][asset] > 0 and df.loc[today - 1][asset] > 0:
+                available_assets.append(asset)
+        returns = input_returns.loc[:today, available_assets]
+    return returns, available_assets
+
+
+
+
+
+
+
+
--- error_optimization.py
+++ error_optimization.py
@@ -1,77 +1,85 @@
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import covariance as cv
-import HMM as hmm
-#from hmmlearn import hmm
-from sklearn.utils import check_random_state
+from HMM import expectation_maximization
+import data_processing as dp
 
 if __name__ == '__main__':
 
     # SET PARAMETERS
+    begin_date = '1993-01-01'
+    end_date = '2002-12-31'
     risk_aversion = 1
-    window = 52
-    HMM_parameters = [[2, 0.1, 50],[3, 0.1, 50],[4, 0.1, 50],[5, 0.1, 50],[6, 0.1, 50],[7, 0.1, 50],[8, 0.1, 50]]
+    rebalancing_period = 52
+    rolling_window = False
+    asset_removal = True
+    HMM_parameters = [[2, 0.1, 20],[3, 0.1, 20],[4, 0.1, 20],[5, 0.1, 20],[6, 0.1, 20],[7, 0.1, 20],[8, 0.1, 20],[9, 0.1, 20]]
 
-    # set dates (and freq)
-    dtindex = pd.bdate_range('1993-01-01', '2002-12-31', weekmask='Fri', freq='C')
-    rebalancing_period = window
-    rebalancing_dates = dtindex[window - 1::rebalancing_period]
+    # Data Processing
+    dtindex = pd.bdate_range(begin_date, end_date, weekmask='Fri', freq='C')
     df = pd.read_csv('markets_new.csv', delimiter=',')
-    df0 = pd.DataFrame(data=df.values, columns=df.columns, index=pd.to_datetime(df['Date']))
-    df0 = df0.reindex(dtindex)
-    df0 = df0.drop(columns=['Date'])
+    df0 = dp.database_processing(df, dtindex, asset_removal)
+
+    # Returns Processing
     input_returns = df0.pct_change().fillna(0)
     input_returns = input_returns.iloc[1:, :]
     num_assets = len(input_returns.columns)
-    # Set Error Vector
-    error = []
+    rebalancing_dates = dtindex[rebalancing_period - 1::rebalancing_period]
+
+    # Set Vectors for Errors
+    mean_error = []
+    max_error = []
     sign = []
 
     for parameters in HMM_parameters:
-        good_sign = 0
-        er_value = 0
-        counter = 0
-        K = parameters[0]
-        p = parameters[1]
-        iter = parameters[2]
-        for date in dtindex[window - 1:]:
+        maximum_error, correct_sign, error_value, counter = 0, 0, 0, 0
+        K, p, iterations = parameters[0], parameters[1], parameters[2]
+
+        for date in dtindex[rebalancing_period - 1:]:
             today = date
-            returns = input_returns.loc[:today, :]  # .tail(window)
-            last = returns.index[-1]
 
             if today in rebalancing_dates:
                 print(today)
-                print(K)
-                # check available assets
-                available_assets = []
-                today_prices = list(df0.loc[today].values)
-                for i in range(0, num_assets):
-                    if today_prices[i] > 0:
-                        available_assets.append(str(i + 1))
-                returns = input_returns.loc[:today, available_assets]
+                print(parameters)
 
-                # model = hmm.GaussianHMM(n_components=K, n_iter=iter)
-                # model.transmat_ = trans_matrix
-                # model.fit(returns)
-                # states = model.predict(returns)
-                # transmat_cdf = np.cumsum(model.transmat_, axis=1)
-                # random_state = check_random_state(model.random_state)
-                # next_state = (transmat_cdf[states[-1]] > random_state.rand()).argmax()
-                # pred_ret = model._generate_sample_from_state(next_state)
+                # Select available assets
+                returns, available_assets = dp.returns_cleaning(input_returns, df0, rolling_window, today)
 
-                posteriori_prob, mu_s, cov_s, pred_ret = hmm.expectation_maximization(returns, K, iter, p)
+                # Returns Predictions
+                posteriori_prob, mu_s, cov_s, pred_ret = expectation_maximization(returns, K, iterations, p)
+
+                # Error Processing
                 actual_ret = input_returns.loc[today+1, available_assets].values
                 abs_error = abs(pred_ret-actual_ret)
                 counter += len(abs_error)
-                er_value += sum(abs_error)
+                error_value += sum(abs_error)
                 for i in range(0, len(abs_error)):
                     if actual_ret[i]*pred_ret[i] >= 0:
-                        good_sign += 1
-        error.append(er_value/counter)
-        sign.append(good_sign/counter)
+                        correct_sign += 1
+                maximum_error = np.max([maximum_error, np.max(abs_error)])
+        max_error.append(maximum_error)
+        mean_error.append(error_value/counter)
+        sign.append(correct_sign/counter)
 
 
+## Ignore this: Just here to print it nicely
+iteration = [1,5,10,15,20,25,30,35,40,45,50]
+k=[2,3,4,5,6,7,8,9]
+plt.figure(figsize=(8,6))
+plt.plot(k,mean_error,'bo')
+plt.xlabel("K")
+plt.ylabel("Mean absolute error")
+plt.savefig("kmean_abs_er.png",bbox_inches='tight')
 
-plt.plot([2,3,4,5,6,7,8],error,'bo')
+plt.figure(figsize=(8,6))
+plt.plot(k, sign,'bo')
+plt.ylabel("Sign prediction accuracy")
+plt.xlabel("K")
+plt.savefig("ksign_accuracy.png",bbox_inches='tight')
+
+
+plt.figure(figsize=(8,6))
+plt.plot(k,max_error,'bo')
+plt.xlabel("K")
+plt.ylabel("Maximum absolute error")
+plt.savefig("kmax_abs_er.png",bbox_inches='tight')
